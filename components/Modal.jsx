8d5004d311f01dd046692c2cc715ff96bfb48db8
Julia Rudnyk <jrudnyk@worth.systems>
2023-09-19 17:46:41 +0200

    Adjust padding of email and phone number fields

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -83,7 +83,7 @@ export default function Modal({isOpen, closeModal, templateId, templateType, tem
                                 onChange={(e) => setPhoneNumber(e.target.value)}
                                 name="phoneNumber"
                                 key="phoneNumber"
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     }
@@ -99,7 +99,7 @@ export default function Modal({isOpen, closeModal, templateId, templateType, tem
                                 onChange={(e) => setEmailAddress(e.target.value)}
                                 name="emailAddress"
                                 key="emailAddress"
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     }
